b5aff5505d2220886f2d6f85a88f1761505d0fd1
Gemma <gemmalowe@gmail.com>
2019-02-06 20:55:18 +0000

    serverless setup from serverless-http docs, and PUT endpoint

--- tasks.js
+++ tasks.js
@@ -34,7 +34,29 @@ app.get('/tasks', function (request, response) {
 app.post('/tasks', function (request, response) {
 
     const answer = {
-        message: "Your task was posted"
+        message: "Your POST request was accepted"
+    }
+
+    response.json(answer);
+})
+
+app.put('/tasks/:taskId', function (request, response) {
+
+    const taskId = request.params.taskId;
+
+    const answer = {
+        message: "Your PUT request was accepted with task ID: " + taskId
+    }
+
+    response.json(answer);
+})
+
+app.delete('/tasks/:taskId', function (request, response) {
+
+    const taskId = request.params.taskId;
+
+    const answer = {
+        message: "Your DELETE request was accepted with task ID: " + taskId
     }
 
     response.json(answer);
